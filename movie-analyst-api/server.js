d5db7c4379e9e21c63a3074a619f40717d0efe9e
Alexandra Swart <answart@sbcglobal.net>
2017-12-29 15:58:04 -0800

    Update for syntax consistency

--- movie-analyst-api/server.js
+++ movie-analyst-api/server.js
@@ -27,7 +27,7 @@ var guard = function(req, res, next){
         if(req.user.scope.includes(permissions[i])){
           next();
         } else {
-          res.send(403, {message: 'Forbidden' });
+          res.status(403).send({ message: 'Forbidden' });
         }
       }
       break;
@@ -39,7 +39,7 @@ var guard = function(req, res, next){
         if(req.user.scope.includes(permissions[i])){
           next();
         } else {
-          res.send(403, {message: 'Forbidden' });
+          res.status(403).send({ message: 'Forbidden' });
         }
       }
       break;
