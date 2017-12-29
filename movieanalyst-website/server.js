43a622897396968f191737630b1459b05e22e801
Alexandra Swart <answart@sbcglobal.net>
2017-12-29 15:09:41 -0800

    Connect website server to views

--- movieanalyst-website/server.js
+++ movieanalyst-website/server.js
@@ -4,6 +4,11 @@ var app = express();
 
 
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/public/views/');
+
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function(req, res){
   res.render('index');
@@ -11,31 +16,43 @@ app.get('/', function(req, res){
 
 
 app.get('/movies', function(req, res){
+  console.log('CALLING /movies:');
   // request
-  //   .get()
+  //   .get('http://localhost:8080/authors')
   //   .set()
   //   .end(function(err, data) {})
+  var movies = []
+  res.render('movies', { movies: movies } );
 })
 
 app.get('/authors', function(req, res){
+  console.log('CALLING /authors:');
   // request
-  //   .get()
+  //   .get('http://localhost:8080/authors')
   //   .set()
   //   .end(function(err, data) {})
+  var authors = []
+  res.render('authors', { authors: authors } );
 })
 
 app.get('/publications', function(req, res){
+  console.log('CALLING /publications:');
   // request
-  //   .get()
+  //   .get('http://localhost:8080/authors')
   //   .set()
   //   .end(function(err, data) {})
+  var publications = []
+  res.render('publications', { publications: publications } );
 })
 
 app.get('/pending', function(req, res){
+  console.log('CALLING /pending:');
   // request
-  //   .get()
+  //   .get('http://localhost:8080/authors')
   //   .set()
   //   .end(function(err, data) {})
+  var pending = []
+  res.render('pending', { pending: pending } );
 })
 
 
